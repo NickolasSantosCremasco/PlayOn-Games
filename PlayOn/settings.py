395f2b42969ca0780f69ea9a84d5fbb5b3228116
Nickolas Cremasco <nickolascremasco@gmail.com>
2024-03-03 15:48:24 -0300

    layout

--- PlayOn/settings.py
+++ PlayOn/settings.py
@@ -117,14 +117,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static'),
     BASE_DIR / 'base_templates' 
 ]
 
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static_root'
 
 MEDIA_URL = '/media/'
 
